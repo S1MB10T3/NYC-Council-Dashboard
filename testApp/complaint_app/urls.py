a724f688bc5cb0932c1df31b51ef9bf054f3fb8d
S1MB10T3 <s1mb10t3@pm.me>
2020-03-01 15:32:52 -0500

    Mentioned Url Fix

--- testApp/complaint_app/urls.py
+++ testApp/complaint_app/urls.py
@@ -2,11 +2,15 @@ from django.urls import path
 from rest_framework import routers
 from .views import ComplaintViewSet, OpenCasesViewSet, ClosedCasesViewSet, TopComplaintTypeViewSet
 
+"""
+Fix:
+    Fixed url issue where endpoints returned just a "detial: none" as a result
+"""
 router = routers.SimpleRouter()
 router.register(r'^', ComplaintViewSet, base_name='complaint')
 router.register(r'^openCases', OpenCasesViewSet, base_name='openCases')
 router.register(r'^closedCases', ClosedCasesViewSet, base_name='closedCases')
 router.register(r'^topComplaints', TopComplaintTypeViewSet, base_name='topComplaints')
-urlpatterns = [
-]
+
+urlpatterns = []
 urlpatterns += router.urls
